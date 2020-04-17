a380fc7fb3e4b64d4dc17ca61287aa4f8129ac45
Krzysztof Kania <kontakt@krzysztofkania.com>
2020-04-17 23:37:27 +0200

    Implement explicit converting

--- converter.js
+++ converter.js
@@ -4,7 +4,8 @@ class converter {
     }
     static multipliers = [
         {name: "k", value: 1024},
-        {name: "M", value: 1024*1024}
+        {name: "M", value: 1024*1024},
+        {name: "", value: 1}
     ];
     
     static units = [
@@ -13,9 +14,17 @@ class converter {
         {name: "W", value: 64},
     ];
 
+    static sorter(a, b){
+        if(a.amount > b.amount){
+            return 1;
+        }else if(a.amount == b.amount){
+            return (a.unit > b.unit);
+        }else{
+            return -1;
+        }
+    }
     
     static convertToBits(amount, unit){
-        
         const convertedToBits = [this.multipliers, this.units].reduce(
                 (previousAmount, currentSet) => converter.reduceAmount(previousAmount, unit, currentSet)
             , amount);
@@ -24,19 +33,29 @@ class converter {
 
     static reduceAmount(amount, unit, set){
         const factor = set.find(m => unit.includes(m.name));
-        if(typeof factor === 'undefined'){
-            return amount;
-        }
         return amount * factor.value;
     }
+
+    static filterFittingMultipliers(amount, set){
+        return  set.filter(m => (amount % m.value) == 0);
+    }
+    
 
     static convertExplicit(amount, unit) {
         const amountOfBits = converter.convertToBits(amount, unit);
 
-        const example = [
-            {amount: 1, unit: "B", asPower:3}
-        ];
-        return example;
+        const values = converter.filterFittingMultipliers(amountOfBits, this.units)
+            .map(unit => 
+                converter.filterFittingMultipliers(amountOfBits/unit.value, this.multipliers)
+                    .map(multi => {
+                        return {
+                            amount: (amountOfBits/unit.value)/multi.value,
+                            unit: multi.name + unit.name,
+                            asPower: Math.log2((amountOfBits/unit.value)/multi.value)
+                            };
+                    })
+            ).reduce((a, b) => a.concat(b));
+        return values.sort(converter.sorter);
     };
 }
 
--- converter.test.js
+++ converter.test.js
@@ -1,6 +1,6 @@
 const converter = require('./converter');
 
-
+const sorter = converter.sorter;
 test('reduce amount by given unit', () => {
     expect(converter.reduceAmount(16, "b", converter.multipliers)).toBe(16);
     expect(converter.reduceAmount(1, "kb", converter.multipliers)).toBe(1024);
@@ -20,23 +20,39 @@ test('convert amount to bits', () => {
 
 test('covertion from explicit amount', () => {
     const eightBits = [
-        {amount: 1, unit: "B", asPower:3}
-    ];
+        {amount: 8, unit: 'b', asPower: 3},
+        {amount: 1, unit: "B", asPower:0}
+    ].sort(sorter);
+
+
+
     expect(converter.convertExplicit(8, "b")).toStrictEqual(eightBits);
 
     const oneKiloBit = [
+        {amount: 1, unit: "kb", asPower:0},
         {amount: 1024, unit: "b", asPower:10}, 
         {amount: 128, unit: "B", asPower:7}, 
         {amount: 16, unit: "W", asPower:4}
-    ];
+    ].sort(sorter);
     expect(converter.convertExplicit(1, "kb")).toStrictEqual(oneKiloBit);
 
     const fourKiloBytes = [
-        {amount: 32768, unit: "b", asPower:10},
         {amount: 32, unit: "kb", asPower:5},
+        {amount: 32768, unit: "b", asPower:15},
         {amount: 4096, unit: "B", asPower:12}, 
         {amount: 4, unit: "kB", asPower:2}, 
         {amount: 512, unit: "W", asPower:9}
-    ];
+    ].sort(sorter);
     expect(converter.convertExplicit(4, "kB")).toStrictEqual(fourKiloBytes);
+});
+
+test('filter out multipliers that can be applied to given number of bits', () => {
+    const sixteenbit = [{name: "", value: 1}];
+    expect(converter.filterFittingMultipliers(16, converter.multipliers)).toStrictEqual(sixteenbit);
+    const example = [
+        {name: "k", value: 1024},
+        {name: "", value: 1}
+    ];
+    expect(converter.filterFittingMultipliers(1024, converter.multipliers)).toStrictEqual(example);
+
 });
