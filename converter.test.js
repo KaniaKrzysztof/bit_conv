ed7adf2fc657d64db1be8dad621c797f2073d52d
Krzysztof Kania <kontakt@krzysztofkania.com>
2020-04-17 22:05:24 +0200

    Add new testcases for explicit conversion

--- converter.test.js
+++ converter.test.js
@@ -17,4 +17,20 @@ test('covertion from explicit amount', () => {
         {amount: 1, unit: "B", asPower:3}
     ];
     expect(converter.convertExplicit(8, "b")).toStrictEqual(eightBits);
+
+    const oneKiloBit = [
+        {amount: 1024, unit: "b", asPower:10}, 
+        {amount: 128, unit: "B", asPower:7}, 
+        {amount: 16, unit: "W", asPower:4}
+    ];
+    expect(converter.convertExplicit(1, "kb")).toStrictEqual(oneKiloBit);
+
+    const fourKiloBytes = [
+        {amount: 32768, unit: "b", asPower:10},
+        {amount: 32, unit: "kb", asPower:5},
+        {amount: 4096, unit: "B", asPower:12}, 
+        {amount: 4, unit: "kB", asPower:2}, 
+        {amount: 512, unit: "W", asPower:9}
+    ];
+    expect(converter.convertExplicit(4, "kB")).toStrictEqual(fourKiloBytes);
 });
