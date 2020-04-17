b3d68d80a091cd64a75d4b8140d1913e81194aab
Krzysztof Kania <kontakt@krzysztofkania.com>
2020-04-17 22:04:05 +0200

    Add method for reducing amount to converter

--- converter.js
+++ converter.js
@@ -2,7 +2,28 @@ class converter {
     constructor() {
         
     }
+    static multipliers = [
+        {name: "k", value: 1024},
+        {name: "M", value: 1024*1024}
+    ];
+    
+    static units = [
+        {name: "b", value: 1},
+        {name: "B", value: 8},
+        {name: "W", value: 64},
+    ];
+
+    
+    static reduceAmount(amount, unit, set){
+        const factor = set.find(m => unit.includes(m.name));
+        if(typeof factor === 'undefined'){
+            return amount;
+        }
+        return amount * factor.value;
+    }
+
     static convertExplicit(amount, unit) {
+
         const example = [
             {amount: 1, unit: "B", asPower:3}
         ];
