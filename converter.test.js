a699e6a9a496cb56b485f1724dd99ee909e094a3
Krzysztof Kania <kontakt@krzysztofkania.com>
2020-04-17 22:03:07 +0200

    Add test for reducing amount to for given multipliers

--- converter.test.js
+++ converter.test.js
@@ -1,10 +1,17 @@
 const converter = require('./converter');
 
 
+test('reduce amount by given unit', () => {
+    expect(converter.reduceAmount(16, "b", converter.multipliers)).toBe(16);
+    expect(converter.reduceAmount(1, "kb", converter.multipliers)).toBe(1024);
+    expect(converter.reduceAmount(1, "kW", converter.multipliers)).toBe(1024);
+    expect(converter.reduceAmount(2, "MB", converter.multipliers)).toBe(2097152);
+});
+
+
 test('covertion from explicit amount', () => {
     const eightBits = [
         {amount: 1, unit: "B", asPower:3}
     ];
     expect(converter.convertExplicit(8, "b")).toStrictEqual(eightBits);
-
 });
